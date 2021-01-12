298e39a1c52a5aaa2a232fc9a0108207811dc01c
Toadless <thomasmcdonaldoctober@gmail.com>
2021-01-12 20:26:23 +0000

    add files

---
+++ docs/Commands.md
@@ -0,0 +1,102 @@
+---
+id: Commands
+title: Commands
+sidebar_label: Commands
+---
+
+## Moderation Commands
+
+### Ban
+`!ban <user>`
+Bans a user
+
+### Kick
+`!kick <user>`
+Kicks a user
+
+### Warn
+`!warn <user>`
+Warns a user.
+If a user has 3 warns they get banned
+
+## Music Commands
+
+### Join
+`!join`
+Joins your voice channel
+Aliases: [`summon`, `connect`]
+
+### Leave
+`!leave`
+Leaves the voice channel
+Aliases: [`disconnect`]
+
+### Loop
+`!loop`
+Loops the current playing song and is toggleable
+Aliases: [`repeat`]
+
+### Play
+`!play <song>`
+Plays a song to your channel
+
+### Skip
+`!skip`
+Skips to the next song
+
+### Stop
+`!stop`
+Stops the currently playing song
+
+### Queue
+`!queue`
+Shows you the queue
+
+### NowPlaying
+`!nowplaying`
+Shows you the currently playing song
+
+## Role Reactions
+
+### RRAdd
+`!rradd <channelId> <messageId> <roleId>`
+Adds a role reaction to the desired message
+
+### RRRemove
+`!rrremove <channelId> <roleId>`
+Removes a role reaction to the desired message
+
+## Other Commands
+
+### Avatar
+`!avatar <user>`
+Replys with the provided users avatar
+
+### Clear
+`!clear <amount>`
+Amount can between 1-100
+Clears the specified amount of messages
+
+### Help
+`!help`
+Displays the help page
+
+### Info
+`!info`
+Displays the info page
+
+### Invite
+`!invite`
+Generates a guild invite
+
+### Prefix
+`!prefix`
+Displays the guilds current prefix
+
+### Uptime
+`!uptime`
+Displays the bots uptime
+
+### SetupTickets
+`!setuptickets`
+Sets up tickets for the guild
---
+++ website/siteConfig.js
@@ -0,0 +1,84 @@
+
+
+const siteConfig = {
+  title: 'Leaf', // Title for your website.
+  tagline: 'Official Leaf Docs',
+  url: 'https://leaf-bot.xyx', // Your website URL
+  baseUrl: '/', // Base URL for your project */
+  // For github.io type URLs, you would set the url and baseUrl like:
+  //   url: 'https://facebook.github.io',
+  //   baseUrl: '/test-site/',
+
+  // Used for publishing and more
+  projectName: 'Leaf',
+  organizationName: 'Leaf',
+  // For top-level user or org sites, the organization is still the same.
+  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+  //   organizationName: 'JoelMarcey'
+
+  // For no header links in the top nav bar -> headerLinks: [],
+  headerLinks: [
+    { doc: 'Commands', label: 'Docs' },
+  ],
+
+  /* path to images for header/footer */
+  headerIcon: 'img/favicon.ico',
+  footerIcon: 'img/favicon.ico',
+  favicon: 'img/favicon.ico',
+
+  /* Colors for website */
+  colors: {
+    primaryColor: '#2e8555',
+    secondaryColor: '#2c0903',
+  },
+
+  /* Custom fonts for website */
+  /*
+  fonts: {
+    myFont: [
+      "Times New Roman",
+      "Serif"
+    ],
+    myOtherFont: [
+      "-apple-system",
+      "system-ui"
+    ]
+  },
+  */
+
+  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+  copyright: `Copyright © ${new Date().getFullYear()} Toadless & Leaf`,
+
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks.
+    theme: 'default',
+  },
+
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: ['https://buttons.github.io/buttons.js'],
+
+  // On page navigation for the current documentation page.
+  onPageNav: 'separate',
+  // No .html extensions for paths.
+  cleanUrl: true,
+
+  // Open Graph and Twitter card images.
+  ogImage: 'img/undraw_online.svg',
+  twitterImage: 'img/undraw_tweetstorm.svg',
+
+  // For sites with a sizable amount of content, set collapsible to true.
+  // Expand/collapse the links and subcategories under categories.
+  // docsSideNavCollapsible: true,
+
+  // Show documentation's last contributor's name.
+  // enableUpdateBy: true,
+
+  // Show documentation's last update time.
+  // enableUpdateTime: true,
+
+  // You may provide arbitrary config keys to be used as needed by your
+  // template. For example, if you need your repo's URL...
+  // repoUrl: 'https://github.com/facebook/test-site',
+};
+
+module.exports = siteConfig;
